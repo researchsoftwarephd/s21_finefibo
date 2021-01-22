47b2e6357819bca8a356b78fdf0ce1682cdc2c95
Nuno Fachada <faken@fakenmc.com>
2021-01-22 12:54:07 +0000

    Put actual author in setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name = "finefibo",
     version = "0.1.0",
-    author = "John Smith",
-    author_email = "john.smith@ulusofona.pt",
+    author = "Nuno Fachada",
+    author_email = "nuno.fachada@ulusofona.pt",
     description = "Fine Fibonacci numbers!",
     long_description = long_description,
     long_description_content_type='text/markdown',
-    url = "https://github.com/jsmith/finefibo",
+    url = "https://github.com/fakenmc/finefibo",
     # Automate below with
     # setuptools.find_packages()
     packages = ['finefibo'],
     # We don't actually need numpy
-    install_requires = ['numpy'],
+    # install_requires = ['numpy'],
     python_requires = '>=3.6',
 )
